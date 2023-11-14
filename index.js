f43b5dc742bfc21f0bf14d804e0818d2b5e6a534
tornikearjevanidze <150293719+tornikearjevanidze@users.noreply.github.com>
2023-11-14 22:39:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let player= {
 
 let cards =[]
 let sum = 0
-let hasBlackJack = false
+let hasBlackJack = true
 let isAlive = false
 let message = " "
 let messageEl = document.getElementById("message-el")
